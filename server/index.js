d55b97227a98b9845f06c11d0fc02e53ce320799
AsadEducation <AsadEducation@outlook.com>
2025-05-13 21:49:16 +0600

    added monogoDB URI

--- server/index.js
+++ server/index.js
@@ -1,37 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ueh5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
-
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
-    try {
-
-       
-
-
-    } finally {
-
-    }
+  try {
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Server ')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Server ");
+});
 
 app.listen(port, () => {
-    console.log(`server is running properly at : ${port}`);
-})
-
+  console.log(`server is running properly at : ${port}`);
+});
